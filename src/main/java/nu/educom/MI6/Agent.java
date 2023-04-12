4bbd9487e16964e3bb0879396151f1d6695ae109
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2023-04-12 12:00:45 +0200

    01. Eerste console opdracht#2

--- src/main/java/nu/educom/MI6/Agent.java
+++ src/main/java/nu/educom/MI6/Agent.java
@@ -3,9 +3,9 @@ package nu.educom.MI6;
 public class Agent {
   private String name;
 
-  private int  serviceNumber;
+  private String  serviceNumber;
   private boolean canLogIn;
-  public Agent(String name, int serviceNumber, boolean canLogin) {
+  public Agent(String name, String serviceNumber, boolean canLogin) {
     this.name = name;
     this.serviceNumber = serviceNumber;
     this.canLogIn = canLogin;
@@ -28,11 +28,11 @@ public class Agent {
     this.name = name;
   }
 
-  public int getServiceNumber() {
+  public String getServiceNumber() {
     return serviceNumber;
   }
 
-  public void setServiceNumber(int serviceNumber) {
+  public void setServiceNumber(String serviceNumber) {
     this.serviceNumber = serviceNumber;
   }
 
--- src/main/java/nu/educom/MI6/Main.java
+++ src/main/java/nu/educom/MI6/Main.java
@@ -5,60 +5,59 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-  public static void main(String[] args) {
-    List<String> blacklist = new ArrayList<>();
-    List<Agent> agents = new ArrayList<>();
-    blacklist.add("FakeAgent");
-    while(true) {
-      for (Agent agent : agents
-      ) {
-        System.out.println(agent.getName() + agent.isCanLogIn());
+  static List<String> blacklist = new ArrayList<>();
+  static List<Agent> agents = new ArrayList<>();
 
-      }
-      Scanner scanner = new Scanner(System.in);
-      System.out.println("Enter your name or enter 'stop' to exit");
-      String name = scanner.nextLine();
-      while (blacklist.contains(name)) {
-        System.out.println("Access denied");
-        System.out.println("Enter another name");
-        name = scanner.nextLine();
-      }
-      if (name.equals("stop")) {
-        System.exit(0);
-      }
-      Scanner serviceNumScan = new Scanner(System.in);
-      int serviceNumber;
-      do {
-        System.out.println("Enter your (positive) service number");
+  public static void main(String[] args) {
+    while (true) {
+      String name = checkName();
+      int serviceNumber = checkServiceNumber();
+      checkSecretCode(name, serviceNumber);
+    }
+  }
+
+  public static String checkName() {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter your name or enter 'stop' to exit");
+    String name = scanner.nextLine();
+    while (blacklist.contains(name)) {
+      System.out.println("Access denied");
+      System.out.println("Enter another name");
+      name = scanner.nextLine();
+    }
+    if (name.equalsIgnoreCase("stop")) {
+      System.exit(0);
+    }
+    return name;
+  }
+
+  public static int checkServiceNumber() {
+    Scanner serviceNumScan = new Scanner(System.in);
+    int serviceNumber;
+    do {
+      System.out.println("Enter your (positive) service number between 1 and 956");
       while (!serviceNumScan.hasNextInt()) {
         System.out.println("Please enter a number");
         serviceNumScan.next();
       }
-        serviceNumber = serviceNumScan.nextInt();
-      } while (serviceNumber <= 0);
-        int length = (int) Math.log10(serviceNumber) + 1;
-        while (length > 3) {
-          System.out.println("Too long, enter a new one");
-          serviceNumber = scanner.nextInt();
-          length = (int) Math.log10(serviceNumber) + 1;
-        }
+      serviceNumber = serviceNumScan.nextInt();
+    } while (serviceNumber <= 0 || serviceNumber > 956);
+    return serviceNumber;
+  }
 
-      System.out.println("Enter the secret code");
-      String userCode = scanner.nextLine();
-      if (userCode.equals("test")) {
-        agents.add(new Agent(name, serviceNumber, true));
-        System.out.printf("You have been logged in with your service number %d, agent %s", serviceNumber, name);
-        System.out.println();
-      } else {
-        System.out.println("Wrong!");
-        blacklist.add(name);
-      }
-
-    }
-
-
-
+  public static void checkSecretCode(String name, int serviceNumber) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter the secret code");
+    String userCode = scanner.nextLine();
+    if (userCode.equals("For ThE Royal QUEEN")) {
+      agents.add(new Agent(name, String.format("%03d", serviceNumber), true));
+      System.out.printf("You have been logged in with your service number %s, agent %s", String.format("%03d", serviceNumber), name);
+      System.out.println();
+    } else {
+      System.out.println("Wrong!");
+      blacklist.add(name);
     }
   }
+}
 
 
